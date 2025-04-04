f5196e8fed683612435851202292d8f36c651811
Nebenmor <nebenmor.anthony@gmail.com>
2025-04-04 11:03:09 +0100

    implement Home Link for Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,13 +22,13 @@ const Navbar = () => {
           </Link>
           <div className="md:ml-auto">
             <div className="flex space-x-2">
-              <a
+              <Link
                 href="/"
                 className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >Home</a
               >
-              <a
-                href="/jobs"
+              <Link
+                to="/jobs"
                 className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >Jobs</a
               >
